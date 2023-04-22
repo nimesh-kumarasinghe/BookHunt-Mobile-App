279e6b52d4a8b891f4f82d50e81fbc4f73fac9ed
nimesh-kumarasinghe <ndilanjana6@gmail.com>
2023-04-22 23:04:49 +0530

    nimesh 4.22 0.4

--- app/src/main/java/com/example/myapplication/Signin.kt
+++ app/src/main/java/com/example/myapplication/Signin.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -26,8 +27,8 @@ class Signin : AppCompatActivity() {
         var txt_sign_up = findViewById<TextView>(R.id.txt_dont_have_anaccount)
         var btn_b= findViewById<Button>(R.id.btn_sign_in_btn)
 
-//        var txt_email = findViewById<EditText>(R.id.edittext_email_email)
-//        var txt_password = findViewById<EditText>(R.id.edittxt_password_pw)
+       var txt_email = findViewById<EditText>(R.id.edittext_email_email)
+        var txt_password = findViewById<EditText>(R.id.edittxt_password_pw)
 
         txt_forgot_pwd.setOnClickListener()
         {
@@ -60,14 +61,86 @@ class Signin : AppCompatActivity() {
 //            queue.add(stringRequest)
             var bynw = Intent(this@Signin, HomeScreen::class.java)
             startActivity(bynw)
+
+            var validatex: Boolean = true
+
+            if (Validatetxt(txt_email) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Email Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(txt_password) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Password Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (emailValidate(txt_email) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Invalid email address",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (passwordValidate(txt_password) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Your password should be more than 8 characters",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (validatex == true){
+                Toast.makeText(
+                    applicationContext, "Success",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                //Signin details here
+            }
+
+
         }
 
-
-
-
-
-
-
-
     }
+    fun Validatetxt(textx: EditText): Boolean {
+
+        var res: Boolean = true
+        if (textx.text.length == 0) {
+            res = false
+        }
+        return res
+    }
+    fun emailValidate(emailtxt: EditText): Boolean {
+        var res: Boolean = true
+
+        var emailx = emailtxt.text
+
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(emailx).matches() == false) {
+            res = false
+        }
+
+        return res
+    }
+    fun passwordValidate(pwdtxt: EditText): Boolean {
+        var res: Boolean = true
+        var pwdx = pwdtxt.text
+
+        if (pwdx.length < 8) {
+            res = false
+        }
+
+        return res
+    }
+
 }
--- app/src/main/java/com/example/myapplication/Signup.kt
+++ app/src/main/java/com/example/myapplication/Signup.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,16 @@ class Signup : AppCompatActivity() {
         supportActionBar?.hide()
 
         var txt_already_acc = findViewById<TextView>(R.id.txt_already_have_an_account)
+
+        var txt_first_name = findViewById<EditText>(R.id.edttxt_fname)
+        var txt_last_name = findViewById<EditText>(R.id.edttxt_lname)
+        var address_no = findViewById<EditText>(R.id.edttxt_address)
+        var city = findViewById<EditText>(R.id.edttxt_city)
+        var street = findViewById<EditText>(R.id.signup_editTxt5)
+        var postal_code = findViewById<EditText>(R.id.edttxt_postal_code)
+        var number = findViewById<EditText>(R.id.edttxt_phoneno)
+        var email = findViewById<EditText>(R.id.edttxt_email)
+        var password = findViewById<EditText>(R.id.edttxt_pw)
 
         txt_already_acc.setOnClickListener()
         {
@@ -26,7 +38,174 @@ class Signup : AppCompatActivity() {
         {
             var bb = Intent(this@Signup, Signin::class.java)
             startActivity(bb)
+
+
+            var validatex: Boolean = true
+
+            if (Validatetxt(txt_first_name) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "First Name Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(txt_last_name) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Last Name Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(address_no) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Address Number Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(city) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "City Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(street) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Street Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(postal_code) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Postal Code Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(number) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Mobile Number Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (mobileValidate(number) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Please recheck your mobile number",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            if (Validatetxt(email) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Email Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (emailValidate(email) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Invalid email address",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (Validatetxt(password) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Password Cannot Be Empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (passwordValidate(password) == false) {
+                if (validatex == true) {
+                    validatex = false
+                    Toast.makeText(
+                        applicationContext, "Your password should be more than 8 characters",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            if (validatex == true){
+                Toast.makeText(
+                    applicationContext, "Success",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                //Signup details here
+            }
+
         }
 
+    }
+    fun Validatetxt(textx: EditText): Boolean {
+
+        var res: Boolean = true
+        if (textx.text.length == 0) {
+            res = false
+        }
+        return res
+    }
+
+    fun mobileValidate(numtxt: EditText): Boolean {
+        var res: Boolean = true
+
+        var xx = numtxt.text
+
+        val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+        if (xx.matches(regex) != true) {
+            res = false
+        }
+        if (xx.length != 10) {
+            res = false
+        }
+
+        return res
+    }
+    fun emailValidate(emailtxt: EditText): Boolean {
+        var res: Boolean = true
+
+        var emailx = emailtxt.text
+
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(emailx).matches() == false) {
+            res = false
+        }
+
+        return res
+    }
+    fun passwordValidate(pwdtxt: EditText): Boolean {
+        var res: Boolean = true
+        var pwdx = pwdtxt.text
+
+        if (pwdx.length < 8) {
+            res = false
+        }
+
+        return res
     }
 }
